a2f09709cdee89dd2ec7ff51920fdf36797ae9eb
diego.lobo <diegolclobo@hotmail.com>
2025-04-13 16:35:24 -0300

    Add project files.

---
+++ src/MiniLojaVirtual.Web/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseMigrationsEndPoint();
+}
+else
+{
+	app.UseExceptionHandler("/Home/Error");
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	"default",
+	"{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
