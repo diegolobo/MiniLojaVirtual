b68c791577e62c94347b0064cda3bd1ac4b5e57d
diego.lobo <diegolclobo@hotmail.com>
2025-04-27 19:13:52 -0300

    test: add architecture validation tests
    Adiciona a classe de testes `ArchitectureTests` para validar as dependências arquiteturais entre as camadas da aplicação;
    Atualiza o arquivo `MiniLojaVirtual.Tests.Architecture.csproj` para incluir referências de pacotes necessárias para testes e define o framework de destino como `net8.0`;
    Marca o projeto como um projeto de teste.

---
+++ tests/MiniLojaVirtual.Tests.Architecture/ArchitectureTests.cs
@@ -0,0 +1,158 @@
+﻿using MiniLojaVirtual.Domain.Models.User;
+using MiniLojaVirtual.Infrastructure.Contexts;
+
+using NetArchTest.Rules;
+
+namespace MiniLojaVirtual.Tests.Architecture;
+
+public class ArchitectureTests
+{
+	[Fact]
+	public void InfrastructureMustDependOnDomain()
+	{
+		var result = Types.InAssembly(typeof(ApplicationDbContext).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Infraestrutura")
+			.Should()
+			.HaveDependencyOn("MiniLojaVirtual.Domain")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful, "A camada de infraestrutura deve depender da camada de domínio.");
+	}
+
+	[Fact]
+	public void InfrastructureMustNotDependOnWebApp()
+	{
+		var result = Types.InAssembly(typeof(ApplicationDbContext).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Infrastructure")
+			.Should()
+			.NotHaveDependencyOn("MiniLojaVirtual.Web")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful,
+			"A camada de infraestrutura não deve depender da camada de interface da aplicação web.");
+	}
+
+	[Fact]
+	public void InfrastructureMustNotDependOnApi()
+	{
+		var result = Types.InAssembly(typeof(ApplicationDbContext).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Infrastructure")
+			.Should()
+			.NotHaveDependencyOn("MiniLojaVirtual.Api")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful,
+			"A camada de infraestrutura não deve depender da camada de interface da api.");
+	}
+
+	[Fact]
+	public void DomainDoesNotDependOnInfrastructure()
+	{
+		var result = Types.InAssembly(typeof(ApplicationUser).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Domain")
+			.Should()
+			.NotHaveDependencyOn("MiniLojaVirtual.Infraestrutura")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful, "A camada de domínio não deve depender da camada de infrastrutura.");
+	}
+
+	[Fact]
+	public void DomainMustNotDependOnApplication()
+	{
+		var result = Types.InAssembly(typeof(ApplicationUser).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Domain")
+			.Should()
+			.NotHaveDependencyOn("MiniLojaVirtual.Application")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful, "A camada de domínio não deve depender da camada de aplicação.");
+	}
+
+	[Fact]
+	public void DomainMustNotDependOnWebApp()
+	{
+		var result = Types.InAssembly(typeof(ApplicationUser).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Domain")
+			.Should()
+			.NotHaveDependencyOn("MiniLojaVirtual.Web")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful,
+			"A camada de domínio não deve depender da camada de interface da aplicação web.");
+	}
+
+	[Fact]
+	public void DomainMustNotDependOnApi()
+	{
+		var result = Types.InAssembly(typeof(ApplicationUser).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Domain")
+			.Should()
+			.NotHaveDependencyOn("MiniLojaVirtual.Api")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful,
+			"A camada de domínio não deve depender da camada de interface da api.");
+	}
+
+	[Fact]
+	public void ApplicationMustDependOnDomain()
+	{
+		var result = Types.InAssembly(typeof(ApplicationUser).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Application")
+			.Should()
+			.HaveDependencyOn("MiniLojaVirtual.Domain")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful, "A camada de aplicação deve depender da camada de domínio.");
+	}
+
+	[Fact]
+	public void ApplicationMustNotDependOnInfrastructure()
+	{
+		var result = Types.InAssembly(typeof(ApplicationUser).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Application")
+			.Should()
+			.NotHaveDependencyOn("MiniLojaVirtual.Infraestrutura")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful, "A camada de aplicação não deve depender da camada de infraestrutura.");
+	}
+
+	[Fact]
+	public void ApplicationMustNotDependOnWebApp()
+	{
+		var result = Types.InAssembly(typeof(ApplicationUser).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Application")
+			.Should()
+			.NotHaveDependencyOn("MiniLojaVirtual.Web")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful,
+			"A camada de aplicação não deve depender da camada de interface da aplicação web.");
+	}
+
+	[Fact]
+	public void ApplicationMustNotDependOnApi()
+	{
+		var result = Types.InAssembly(typeof(ApplicationUser).Assembly)
+			.That()
+			.ResideInNamespace("MiniLojaVirtual.Application")
+			.Should()
+			.NotHaveDependencyOn("MiniLojaVirtual.Api")
+			.GetResult();
+
+		Assert.True(result.IsSuccessful,
+			"A camada de aplicação não deve depender da camada de interface da api.");
+	}
+}
